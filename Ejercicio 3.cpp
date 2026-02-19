55f0e328a9dc3ce66b97335860e91cf6aab35493
toya <emontoyas1@eafit.edu.co>
2026-02-19 06:44:04 -0500

    agregue un algoritmo de ordenamiento, tambien se agrego el chrono.

---
+++ Ejercicio 3.cpp
@@ -0,0 +1,3 @@
+//
+// Created by monto on 11/02/2026.
+//
--- ej1.cpp
+++ ej1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <chrono>
 
 
 using namespace std;
@@ -12,7 +13,7 @@ int* crearArray(int n) {
 
 int* llennarArray(int arr[], int n) {
     for (int i = 0; i < n; ++i) {
-        arr[i] = rand() % 10000;
+        arr[i] = rand() % 1000;
     }
     return arr;
 }
@@ -34,6 +35,7 @@ void buscarMayor(int arr[], int n) {
             mayorActual = mayorActual;
         }
     }
+
 std::cout<< "Mayor:  " <<mayorActual <<endl;
 }
 
@@ -50,20 +52,40 @@ void buscarMenor(int arr[], int n) {
     std::cout<< "Menor:  " <<menorActual <<endl;
 }
 
-
+void sortArray(int arr[], int n ) {
+    for (int i=1; i<n; i++) {
+        for (int j =0; j<n-i; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " | ";
+    }
+    cout << endl;
+}
 
 
     int main() {
+
     int n;
     std::cout << "ingresa la magnitud del arreglo: ";
     std::cin >> n;
-
+    auto inicio = chrono::high_resolution_clock::now();
 int* arr = crearArray(n);
 llennarArray(arr, n);
 imprimirArray(arr, n);
     buscarMayor(arr,n);
     buscarMenor(arr,n);
+    sortArray(arr,n);
     delete[] arr;
+
+    auto fin = chrono::high_resolution_clock::now();
+    chrono::duration<double> tiempo = fin - inicio;
+    cout << "Tiempo de ejecucion: " << tiempo.count() << " segundos" << endl;
     return 0;
 }
 
